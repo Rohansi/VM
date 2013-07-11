bef97604c13dc306ea2ed0fcbcfeb44af356f2a5
Zach Reedy <paraton@hotmail.com>
2013-07-11 03:31:32 -0400

    Include preprocessor added
    Also cleaned up the tokenizer a bit.

--- Assembler/AssemblyTokenizer.cs
+++ Assembler/AssemblyTokenizer.cs
@@ -1,51 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Assembler
 {
-    public enum TokenType
-    {
-        EndOfFile, Number, String,
-        Word, Keyword, Label, Comma, OpenBracket, CloseBracket, Period,
+	public enum TokenType
+	{
+		EndOfFile, Number, String,
+		Word, Keyword, Label, Comma, OpenBracket, CloseBracket, Period,
 		OpenParentheses, CloseParentheses,
 		Add, Subtract, Multiply, Divide, Modulo,
 		BitwiseNot, BitwiseAnd, BitwiseOr, BitwiseXor
-    }
+	}
 
-    public struct Token
-    {
-        public readonly TokenType Type;
-        public readonly int Line;
-        public readonly string Value;
+	public struct Token
+	{
+		public readonly TokenType Type;
+		public readonly string Filename;
+		public readonly int Line;
+		public readonly string Value;
 
-        public Token(TokenType type, string value = null, int line = -1)
-        {
-            Type = type;
-            Line = line;
-            Value = value;
-        }
-    }
+		public Token(TokenType type, string value = null, int line = -1, string filename = "")
+		{
+			Type = type;
+			Line = line;
+			Filename = filename;
+			Value = value;
+		}
+	}
 
-    public class AssemblyTokenizer
-    {
-        private readonly string source;
-        private readonly List<Token> tokens;
-        private bool hasTokenized;
-	    private readonly Dictionary<string, List<Token>> defines; 
+	public class AssemblyTokenizer
+	{
+		private readonly string source;
+		private readonly List<Token> tokens;
+		private bool hasTokenized;
+		private readonly Dictionary<string, List<Token>> defines;
 
-        public AssemblyTokenizer(string input)
-        {
-            source = input;
-            tokens = new List<Token>();
+		public AssemblyTokenizer(string input)
+		{
+			source = input;
+			tokens = new List<Token>();
 			defines = new Dictionary<string, List<Token>>();
-        }
+		}
 
-        public TokenList<Token> Tokens
-        {
-            get
-            {
-                if (!hasTokenized)
-                    throw new InvalidOperationException("Scan() has not been called");
+		public TokenList<Token> Tokens
+		{
+			get
+			{
+				if (!hasTokenized)
+					throw new InvalidOperationException("Scan() has not been called");
 
 				if (tokens.Count > 0)
 				{
@@ -57,118 +60,122 @@ namespace Assembler
 					var end = new Token(TokenType.EndOfFile, line: 1);
 					return new TokenList<Token>(tokens, end);
 				}
-            }
-        }
+			}
+		}
 
-        public void Scan()
-        {
-            if (hasTokenized)
-                throw new InvalidOperationException("Scan() has already been called");
+		public void Scan()
+		{
+			if (hasTokenized)
+				throw new InvalidOperationException("Scan() has already been called");
 
-            var tokenizer = new Tokenizer(source);
-            tokenizer.Scan();
+			var tokenizer = new Tokenizer(source);
+			tokenizer.Scan();
 
-            var t = tokenizer.Tokens;
-            for (var i = 0; t[i].Type != BasicTokenType.EndOfFile; i++)
-            {
-                var tok = t[i];
+			ScanSource(tokenizer.Tokens);
 
-                switch (tok.Type)
-                {
-                    case BasicTokenType.Word:
-                    {
-                        switch (tok.Value.ToLower())
-                        {
-                            case "set":
-                            case "add":
-                            case "sub":
-                            case "mul":
-                            case "div":
-                            case "mod":
-                            case "inc":
-                            case "dec":
-                            case "not":
-                            case "and":
-                            case "or":
-                            case "xor":
-                            case "shl":
-                            case "shr":
-                            case "push":
-                            case "pop":
-                            case "jmp":
-                            case "call":
-                            case "ret":
-                            case "in":
-                            case "out":
-                            case "cmp":
-                            case "jz":
-                            case "jnz":
-                            case "je":
-                            case "ja":
-                            case "jb":
-                            case "jae":
-                            case "jbe":
-                            case "jne":
-                                tokens.Add(new Token(TokenType.Keyword, tok.Value.ToLower(), tok.Line));
-                                break;
-                            default:
-								if (defines.ContainsKey(tok.Value))
-								{
-									List<Token> define = defines[tok.Value];
-									foreach(Token token in define)
-										tokens.Add(new Token(token.Type, token.Value, tok.Line));
-								}
-								else
-									tokens.Add(new Token(TokenType.Word, tok.Value, tok.Line));
-                                break;
-                        }
-                        break;
-                    }
+			hasTokenized = true;
+		}
 
-                    case BasicTokenType.Delimiter:
-                    {
-                        if (tok.Value == ",")
-                        {
-                            tokens.Add(new Token(TokenType.Comma, tok.Value, tok.Line));
-                            break;
-                        }
+		private void ScanSource(TokenList<BasicToken> sourceTokens)
+		{
+			for (var i = 0; sourceTokens[i].Type != BasicTokenType.EndOfFile; i++)
+			{
+				var token = sourceTokens[i];
 
-                        if (tok.Value == "[")
-                        {
-                            tokens.Add(new Token(TokenType.OpenBracket, tok.Value, tok.Line));
-                            break;
-                        }
-
-                        if (tok.Value == "]")
-                        {
-                            tokens.Add(new Token(TokenType.CloseBracket, tok.Value, tok.Line));
-                            break;
-                        }
-
-                        if (tok.Value == ":" && tokens.Count > 0)
-                        {
-                            var last = tokens[tokens.Count - 1];
-                            if (last.Type == TokenType.Word)
-                            {
-                                tokens.RemoveAt(tokens.Count - 1);
-                                tokens.Add(new Token(TokenType.Label, last.Value, last.Line));
-                                break;
-                            }
-                        }
-
-						if (tok.Value == "#")
+				switch (token.Type)
+				{
+					case BasicTokenType.Word:
+					{
+						HashSet<string> opcodes = new HashSet<string>
 						{
-							tok = t[++i];
-							switch(tok.Value)
+							"set", "add", "sub", "mul", "div", "mod", "inc", "dec", "not", "and", "or", "xor", "shl",
+							"shr", "push", "pop", "jmp", "call", "ret", "in", "out", "cmp", "jz", "jnz", "je", "ja",
+							"jb", "jae", "jbe", "jne"
+						};
+
+						if (opcodes.Contains(token.Value.ToLower()))
+							tokens.Add(new Token(TokenType.Keyword, token.Value.ToLower(), token.Line));
+						else
+						{
+							if (defines.ContainsKey(token.Value))
 							{
+								List<Token> define = defines[token.Value];
+								foreach (Token defineToken in define)
+									tokens.Add(new Token(defineToken.Type, defineToken.Value, defineToken.Line));
+							}
+							else
+								tokens.Add(new Token(TokenType.Word, token.Value, token.Line));
+						}
+						break;
+					}
+
+					case BasicTokenType.Delimiter:
+					{
+						Dictionary<string, TokenType> delimiters = new Dictionary<string, TokenType>
+						{
+							{ ",", TokenType.Comma },
+							{ "[", TokenType.OpenBracket },
+							{ "]", TokenType.CloseBracket },
+							{ "(", TokenType.OpenParentheses },
+							{ ")", TokenType.CloseParentheses },
+							{ ".", TokenType.Period },
+							{ "+", TokenType.Add },
+							{ "-", TokenType.Subtract },
+							{ "*", TokenType.Multiply },
+							{ "/", TokenType.Divide },
+							{ "%", TokenType.Modulo },
+							{ "~", TokenType.BitwiseNot },
+							{ "&", TokenType.BitwiseAnd },
+							{ "|", TokenType.BitwiseOr },
+							{ "^", TokenType.BitwiseXor }
+						};
+
+						if (delimiters.ContainsKey(token.Value))
+						{
+							tokens.Add(new Token(delimiters[token.Value], token.Value, token.Line));
+							break;
+						}
+
+						if (token.Value == ":" && tokens.Count > 0)
+						{
+							var last = tokens[tokens.Count - 1];
+							if (last.Type == TokenType.Word)
+							{
+								tokens.RemoveAt(tokens.Count - 1);
+								tokens.Add(new Token(TokenType.Label, last.Value, last.Line));
+								break;
+							}
+						}
+
+						if (token.Value == "#")
+						{
+							token = sourceTokens[++i];
+							switch (token.Value)
+							{
+								case "include":
+								{
+									string sourceFilename = "";
+									BasicToken filename = sourceTokens[i];
+									while (sourceTokens[++i].Line == filename.Line && i < sourceTokens.Count)
+										sourceFilename += sourceTokens[i].Value;
+
+									string includeSource = File.ReadAllText(sourceFilename.Replace("\"", null));
+									var tokenizer = new Tokenizer(includeSource);
+									tokenizer.Scan();
+
+									ScanSource(tokenizer.Tokens);
+									--i;
+									break;
+								}
+
 								case "define":
 								{
 									List<Token> defineTokens = new List<Token>();
-									BasicToken name = t[++i];
+									BasicToken name = sourceTokens[++i];
 
-									while (t[++i].Line == name.Line)
+									while (sourceTokens[++i].Line == name.Line && i < sourceTokens.Count)
 									{
-										defineTokens.Add(new Token(TokenType.Number, t[i].Value, t[i].Line));
+										defineTokens.Add(new Token(TokenType.Number, sourceTokens[i].Value, sourceTokens[i].Line));
 									}
 
 									defines.Add(name.Value, defineTokens);
@@ -177,134 +184,56 @@ namespace Assembler
 								}
 
 								default:
-									throw new AssemblerException(String.Format("Unexpected preprocessor directive \"{0}\".", tok.Value));
+									throw new AssemblerException(String.Format("Unexpected preprocessor directive \"{0}\".", token.Value));
 							}
 							break;
 						}
 
-						if (tok.Value == "(")
-						{
-							tokens.Add(new Token(TokenType.OpenParentheses, tok.Value, tok.Line));
-							break;
-						}
+						throw new AssemblerException(String.Format("Unexpected delimiter '{0}'", token.Value));
+					}
 
-						if (tok.Value == ")")
-						{
-							tokens.Add(new Token(TokenType.CloseParentheses, tok.Value, tok.Line));
-							break;
-						}
+					case BasicTokenType.Number:
+						tokens.Add(new Token(TokenType.Number, token.Value, token.Line));
+						break;
+						
+					case BasicTokenType.String:
+						tokens.Add(new Token(TokenType.String, token.Value, token.Line));
+						break;
 
-						if (tok.Value == ".")
-						{
-							tokens.Add(new Token(TokenType.Period, tok.Value, tok.Line));
-							break;
-						}
+					default:
+						throw new AssemblerException(String.Format("Unhandled BasicToken {0}", token.Type));
+				}
+			}
+		}
 
-						if (tok.Value == "+")
-						{
-							tokens.Add(new Token(TokenType.Add, tok.Value, tok.Line));
-							break;
-						}
-
-						if (tok.Value == "-")
-						{
-							tokens.Add(new Token(TokenType.Subtract, tok.Value, tok.Line));
-							break;
-						}
-
-						if (tok.Value == "*")
-						{
-							tokens.Add(new Token(TokenType.Multiply, tok.Value, tok.Line));
-							break;
-						}
-
-						if (tok.Value == "/")
-						{
-							tokens.Add(new Token(TokenType.Divide, tok.Value, tok.Line));
-							break;
-						}
-
-						if (tok.Value == "%")
-						{
-							tokens.Add(new Token(TokenType.Modulo, tok.Value, tok.Line));
-							break;
-						}
-
-						if (tok.Value == "~")
-						{
-							tokens.Add(new Token(TokenType.BitwiseNot, tok.Value, tok.Line));
-							break;
-						}
-
-						if (tok.Value == "&")
-						{
-							tokens.Add(new Token(TokenType.BitwiseAnd, tok.Value, tok.Line));
-							break;
-						}
-
-						if (tok.Value == "|")
-						{
-							tokens.Add(new Token(TokenType.BitwiseOr, tok.Value, tok.Line));
-							break;
-						}
-
-						if (tok.Value == "^")
-						{
-							tokens.Add(new Token(TokenType.BitwiseXor, tok.Value, tok.Line));
-							break;
-						}
-
-                        throw new AssemblerException(String.Format("Unexpected delimiter '{0}'", tok.Value));
-                    }
-
-                    case BasicTokenType.Number:
-                    {
-                        tokens.Add(new Token(TokenType.Number, tok.Value, tok.Line));
-                        break;
-                    }
-
-                    case BasicTokenType.String:
-                    {
-                        tokens.Add(new Token(TokenType.String, tok.Value, tok.Line));
-                        break;
-                    }
-
-                    default:
-                    throw new AssemblerException(String.Format("Unhandled BasicToken {0}", tok.Type));
-                }
-            }
-
-            hasTokenized = true;
-        }
-
-	    public static bool IsExpressionOperation(TokenType type)
-	    {
-		    return new System.Collections.Generic.List<TokenType>
+		public static bool IsExpressionOperation(TokenType type)
+		{
+			return new HashSet<TokenType>
 		    {
 			    TokenType.Add,
 			    TokenType.Subtract
 		    }.Contains(type);
-	    }
+		}
 
-	    public static bool IsTermOperation(TokenType type)
-	    {
-		    return new System.Collections.Generic.List<TokenType>
+		public static bool IsTermOperation(TokenType type)
+		{
+			return new HashSet<TokenType>
 		    {
 			    TokenType.Multiply,
 			    TokenType.Divide,
 			    TokenType.Modulo
 		    }.Contains(type);
-	    }
+		}
 
-	    public static bool IsBitwiseOperation(TokenType type)
-	    {
-		    return new System.Collections.Generic.List<TokenType>
+		public static bool IsBitwiseOperation(TokenType type)
+		{
+			return new HashSet<TokenType>
 		    {
 			    TokenType.BitwiseNot,
 			    TokenType.BitwiseAnd,
 			    TokenType.BitwiseOr,
 			    TokenType.BitwiseXor
 		    }.Contains(type);
-	    }
-    }
+		}
+	}
 }
